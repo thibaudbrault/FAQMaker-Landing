ea6173ed703f6a3b77141ef7df72fc9e31fdef46
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-12 19:54:36 +0200

    :heavy_plus_sign: install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), react()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
