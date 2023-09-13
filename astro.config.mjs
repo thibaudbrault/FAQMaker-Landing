1e92b857b68c7072483b42339258d7cae145db5b
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-13 14:26:46 +0200

    :arrow_up: upgrade astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
@@ -8,14 +7,6 @@ export default defineConfig({
   site: "https://faqmaker.co",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     react(),
   ],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
 });
